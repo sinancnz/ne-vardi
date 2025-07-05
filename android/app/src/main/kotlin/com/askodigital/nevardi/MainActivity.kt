12c5e491c9ed76fc7eca699102425271974adc1f
sinancnz <sinan.cengiz.tarabya@gmail.com>
2025-07-05 19:01:08 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/askodigital/nevardi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.askodigital.nevardi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String id;
+  final String name;
+  final int quantity;
+  final DateTime expiry;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.expiry,
+  });
+}
